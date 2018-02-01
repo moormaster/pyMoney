d1951baa004d4859ec674f93c6fe2576de97376d
Andre Herbst <moormaster@gmx.net>
2018-02-01 23:02:01 +0100

    fix: filters for "summary categories" with < operator, i.e. <2018 02

--- pyMoney.py
+++ pyMoney.py
@@ -84,9 +84,9 @@ class PyMoneyConsole(lib.app.PyMoney):
 					transactionfilter = transactionfilter.and_concat(lambda t: t.date.year > year or t.date.year == year and t.date.month > month)
 			if lower:
 				if filter_day:
-					transactionfilter = transactionfilter.and_concat(lambda t: t.date.year > year or t.date.year == year and t.date.month <= month)
+					transactionfilter = transactionfilter.and_concat(lambda t: t.date.year < year or t.date.year == year and t.date.month <= month)
 				else:
-					transactionfilter = transactionfilter.and_concat(lambda t: t.date.year > year or t.date.year == year and t.date.month < month)
+					transactionfilter = transactionfilter.and_concat(lambda t: t.date.year < year or t.date.year == year and t.date.month < month)
 			if equal:
 				transactionfilter = transactionfilter.and_concat(lambda t: t.date.month == month)
 
