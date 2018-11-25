82de6fca1d5c17fc438e1bf2301854eb18af301e
moormaster <moormaster@gmx.net>
2018-11-25 17:47:41 +0100

    refactored unit test test_filter

--- lib/data/test_filter.py
+++ lib/data/test_filter.py
@@ -5,59 +5,41 @@ import unittest
 
 class TestFilter(unittest.TestCase):
 	def setUp(self):
-		self.data = []
-		self.data.append("A1")
-		self.data.append("A2")
-		self.data.append("B1")
-		self.data.append("B2")
+		self.trueFilter = filter.Filter(lambda n: True)
+		self.falseFilter = filter.Filter(lambda n: False)
 
-		self.charAFilter = filter.Filter(lambda v: v[0] == "A")
-		self.num1Filter = filter.Filter(lambda v: v[1] == "1")
-
-	def test_filters(self):
-		self.assertTrue(self.charAFilter(self.data[0]))
-		self.assertTrue(self.charAFilter(self.data[1]))
-		self.assertFalse(self.charAFilter(self.data[2]))
-		self.assertFalse(self.charAFilter(self.data[3]))
-
-		self.assertTrue(self.num1Filter(self.data[0]))
-		self.assertFalse(self.num1Filter(self.data[1]))
-		self.assertTrue(self.num1Filter(self.data[2]))
-		self.assertFalse(self.num1Filter(self.data[3]))
+	def test_default_filter(self):
+		self.assertTrue(self.trueFilter(123))
+		self.assertFalse(self.falseFilter(123))
 
 	def test_or_concat(self):
-		transactionfilter = [self.charAFilter.or_concat(self.num1Filter),
-							 self.num1Filter.or_concat(self.charAFilter)]
+		false_or_false_filter = self.falseFilter.or_concat(self.falseFilter)
+		false_or_true_filter = self.falseFilter.or_concat(self.trueFilter)
+		true_or_false_filter = self.trueFilter.or_concat(self.falseFilter)
+		true_or_true_filter = self.trueFilter.or_concat(self.trueFilter)
 
-		for f in transactionfilter:
-			for d in self.data:
-				if self.charAFilter(d) or self.num1Filter(d):
-					self.assertTrue(f(d))
-				else:
-					self.assertFalse(f(d))
+		self.assertFalse(false_or_false_filter(123), "false or false should equal to false")
+		self.assertTrue(false_or_true_filter(123), "false or true should equal to true")
+		self.assertTrue(true_or_false_filter(123), "true or false should equal to true")
+		self.assertTrue(true_or_true_filter(123), "true or true should equal to true")
 
 	def test_and_concat(self):
-		transactionfilter = [self.charAFilter.and_concat(self.num1Filter),
-							 self.num1Filter.and_concat(self.charAFilter)]
+		false_and_false_filter = self.falseFilter.and_concat(self.falseFilter)
+		false_and_true_filter = self.falseFilter.and_concat(self.trueFilter)
+		true_and_false_filter = self.trueFilter.and_concat(self.falseFilter)
+		true_and_true_filter = self.trueFilter.and_concat(self.trueFilter)
 
-		for f in transactionfilter:
-			for d in self.data:
-				if self.charAFilter(d) and self.num1Filter(d):
-					self.assertTrue(f(d))
-				else:
-					self.assertFalse(f(d))
+		self.assertFalse(false_and_false_filter(123), "false and false should equal to false")
+		self.assertFalse(false_and_true_filter(123), "false and true should equal to false")
+		self.assertFalse(true_and_false_filter(123), "true and false should equal to false")
+		self.assertTrue(true_and_true_filter(123), "true and true should equal to true")
 
 	def test_negate(self):
-		transactionfilter = [self.charAFilter, self.num1Filter]
+		negated_false_filter = self.falseFilter.negate()
+		negated_true_filter = self.trueFilter.negate()
 
-		for f in transactionfilter:
-			nf = f.negate()
-
-			for t in self.data:
-				if not f(t):
-					self.assertTrue(nf(t))
-				else:
-					self.assertFalse(nf(t))
+		self.assertTrue(negated_false_filter(123), "negation of false should be true")
+		self.assertFalse(negated_true_filter(123), "negation of true should be false")
 
 
 class TestFilterIterator(unittest.TestCase):
